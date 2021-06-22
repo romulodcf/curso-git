f591f5e566aebaede9a84338d22cecda255e5140
Romulo Soares <romulodcf@gmail.com>
2021-06-21 22:23:21 -0300

    Add como vc esta

--- projeto-teste/Program.cs
+++ projeto-teste/Program.cs
@@ -5,6 +5,10 @@ namespace projeto_teste {
 		static void Main(string[] args) {
 			Console.WriteLine("Hello World!");
 			Console.WriteLine("Bom dia!");
+			Console.WriteLine("Como você está hoje??");
+
+
+			Console.WriteLine();
 		}
 	}
 }
